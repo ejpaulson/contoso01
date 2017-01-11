b1e2e2837690f84b2f5b7190e578c570760036fb
Emily <epaulson@uw.edu>
2017-01-10 19:54:59 -0800

    First commit.

---
+++ contoso01/Controllers/StoreController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace contoso01.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public string Index()
+        {
+            return "Hi from the Store Controller --> index page";
+        }
+        public string Browse(string genre)
+        {
+            string message =
+                HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+            return message;
+        }
+        public string Details()
+        {
+            return "Hi from the Store Controller --> details page";
+        }
+    }
+}
